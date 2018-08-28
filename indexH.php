7d8b5ad51a7f62b21893d7f015473332eb458948
César Greppin <cesar.greppin@ringier.ch>
2018-08-28 15:58:45 +0200

    added js for _blank links on cards

--- indexH.php
+++ indexH.php
@@ -5,12 +5,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dirs  = array();
 $dirs[] = __DIR__;
 $dirs[] = __DIR__ . '/templates';
-if (is_dir( __DIR__ . '/../../_admin/templates')) {
-	$dirs[] =  __DIR__ . '/../../_admin/templates';
+if (is_dir( __DIR__ . '/../_admin/templates')) {
+	$dirs[] =  __DIR__ . '/../_admin/templates';
 }
 
 if (strpos($_SERVER['HTTP_HOST'], 'localhost') !== FALSE) {
-	$dirs[] = __DIR__ . '/../web/_admin/templates';
+	$dirs[] = __DIR__ . '/../_admin/templates';
 }
 
 $loader = new Twig_Loader_Filesystem($dirs);
@@ -42,12 +42,12 @@ if (isset($_GET['page']) && !empty($_GET['page'])) {
 }
 
 $content = file_get_contents($template);
-if (strpos($content, "block('head_brand')") === false || 
-	strpos($content, "block('bar_brand')") === false || 
-	strpos($content, "block('footer_brand')") === false) {
+// if (strpos($content, "block('head_brand')") === false || 
+// 	strpos($content, "block('bar_brand')") === false || 
+// 	strpos($content, "block('footer_brand')") === false) {
 
-	throw new Exception("Missing mandatory blocks!", 1);
-}
+// 	throw new Exception("Missing mandatory blocks!", 1);
+// }
 
 // Afficher le template
 echo $twig->render($template, $variables);
