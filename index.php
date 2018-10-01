90e0bbf32771320193c9eecd4466f9c4ed3241ac
César Greppin <Citcatjoe@users.noreply.github.com>
2018-10-01 09:34:49 +0200

    w

--- index.php
+++ index.php
@@ -5,12 +5,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dirs  = array();
 $dirs[] = __DIR__;
 $dirs[] = __DIR__ . '/templates';
-if (is_dir( __DIR__ . '/../../_admin/templates')) {
-	$dirs[] =  __DIR__ . '/../../_admin/templates';
+if (is_dir( __DIR__ . '/../_admin/templates')) {
+	$dirs[] =  __DIR__ . '/../_admin/templates';
 }
 
 if (strpos($_SERVER['HTTP_HOST'], 'localhost') !== FALSE) {
-	$dirs[] = __DIR__ . '/../web/_admin/templates';
+	$dirs[] = __DIR__ . '/../_admin/templates';
 }
 
 $loader = new Twig_Loader_Filesystem($dirs);
